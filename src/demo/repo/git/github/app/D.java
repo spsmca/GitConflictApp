9e2a1d47444f5875be4725bdedf3a274a741efda
spsmca <71463984+spsmca@users.noreply.github.com>
2023-01-04 22:14:56 +0530

    Update D.java

--- src/demo/repo/git/github/app/D.java
+++ src/demo/repo/git/github/app/D.java
@@ -4,7 +4,6 @@ public class D {
 
 	public static String getLines() {
 		StringBuffer sb= new StringBuffer();
-		sb.append(" D.First ");
 		sb.append("\n D.V1  ");
 		sb.append("\n D.Last ");
 		return sb.toString();
