1b8ec5a7bc579e6a5ee6b717533cdc4fc92eee50
spsmca <71463984+spsmca@users.noreply.github.com>
2023-01-04 22:08:52 +0530

    Update B.java

--- src/demo/repo/git/github/app/B.java
+++ src/demo/repo/git/github/app/B.java
@@ -5,7 +5,7 @@ public class B {
 	public static String getLines() {
 		StringBuffer sb= new StringBuffer();
 		sb.append(" B.First ");
-		sb.append("\n B.V1  ");
+		sb.append("\n B.V2 Remote  ");
 		sb.append("\n B.Last ");
 		return sb.toString();
 	}
