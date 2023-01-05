1c67fa17364f5b85e4d018fbc9d000852f135f42
spsmca <sps_mca@rediffmail.com>
2023-01-05 16:12:33 +0530

    Conflict Resolved

--- src/demo/repo/git/github/app/B.java
+++ src/demo/repo/git/github/app/B.java
@@ -5,11 +5,7 @@ public class B {
 	public static String getLines() {
 		StringBuffer sb= new StringBuffer();
 		sb.append(" B.First ");
-<<<<<<< HEAD
-		sb.append("\n B.V2-Local  ");
-=======
-		sb.append("\n B.V2Remote  ");
->>>>>>> bc84bb5af0fd3eedf1b6a085d7cdb3ca45226cbf
+		sb.append("\n B.V2-LocalRemote  ");
 		sb.append("\n B.Last ");
 		return sb.toString();
 	}
